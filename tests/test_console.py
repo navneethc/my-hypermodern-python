48bc5d163e5efe28e75887063be350681e8ea777
navneethc <navneethc@gmail.com>
2021-05-12 02:58:25 +0530

    Include tests

---
+++ tests/test_console.py
@@ -0,0 +1,9 @@
+import click.testing
+
+from my_hypermodern_python import console
+
+
+def test_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
