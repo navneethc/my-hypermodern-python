f0734abfea19604a1221720cb5216cd6fd6327e9
navneethc <navneethc@gmail.com>
2021-05-14 03:00:40 +0530

    Mock the Wikipedia API call

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,10 +5,20 @@ from my_hypermodern_python import console
 
 
 @pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch('requests.get')
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+@pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
