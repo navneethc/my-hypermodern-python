8a0c3ab3ef63049de17fba6dc6c8e181e037d5f5
navneethc <navneethc@gmail.com>
2021-05-15 04:00:32 +0530

    Implement Wikipedia language option and tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,6 +10,11 @@ def runner():
     return click.testing.CliRunner()
 
 
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("my_hypermodern_python.wikipedia.random_page")
+
+
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
@@ -41,3 +46,8 @@ def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
+
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ['--language=pl'])
+    mock_wikipedia_random_page.assert_called_with(language='pl')
