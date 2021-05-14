362b9b4aab6de27dcb58f15baca34c80e8674ec7
navneethc <navneethc@gmail.com>
2021-05-15 03:51:32 +0530

    Move mock request object to conftest.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,16 +6,6 @@ from my_hypermodern_python import console
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch('requests.get')
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
-
-
-@pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
