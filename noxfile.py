9a911b776ba38d76c191c47b217b149f79da2f91
navneethc <navneethc@gmail.com>
2021-05-15 13:15:43 +0530

    Add black session

--- noxfile.py
+++ noxfile.py
@@ -16,3 +16,11 @@ def lint(session):
     args = session.posargs or locations
     session.install('flake8')
     session.run('flake8', *args)
+
+
+@nox.session(python=['3.8', '3.7'])
+def black(session):
+    args = session.posargs or locations
+    session.install('black')
+    session.run('black', *args)
+    
