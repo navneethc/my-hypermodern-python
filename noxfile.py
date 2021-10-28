6a9c25d2cea961ce7a4a5c20996a644c511317bf
navneethc <navneethc@gmail.com>
2021-10-29 02:57:52 +0530

    manage dependencies in Nox sessions with Poetry

--- noxfile.py
+++ noxfile.py
@@ -2,22 +2,38 @@ import nox
 import tempfile
 
 nox.options.sessions = "lint", "safety", "tests"
+locations = "src", "tests", "noxfile.py"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
-
-
-locations = "src", "tests", "noxfile.py"
 
 
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -30,8 +46,9 @@ def lint(session):
 @nox.session(python=["3.8", "3.7"])
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
+
 
 @nox.session(python=["3.8"])
 def safety(session):
@@ -45,5 +62,5 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
